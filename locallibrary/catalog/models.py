8ea922119c561837d104f316ff7065fb84d1545c
Robert Morrison <robert.morrison@4-most.co.uk>
2019-12-14 17:44:20 +0000

    Updated potential URLs & created & migrated SQLLite database

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.urls import reverse
+
+import uuid
 
 # Create your models here.
+
+class Genre(models.Model):
+    """model representing a book genre."""
+    name = models.CharField(max_length=200
+            , help_text = 'Enter a book genre (e.g. Science Fiction)'
+            )
+
+    def __str__(self):
+        """string for representing the Model object"""
+        return self.name
+
+class Author(models.Model):
+    """Model representing an author"""
+
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    date_of_birth = models.DateField(null = True, blank = True)
+    date_of_death = models.DateField('Died', null = True, blank = True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance"""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f"{self.last_name}, {self.last_name}"
+
+class Language(models.Model):
+    """Model representing the original language a book was written in"""
+    name = models.CharField(max_length = 100
+            , help_text = "Enter the book's natural language (3.g. French, Englush or Japanese)")
+
+    def __str__(self):
+        """String represeenting the object in Admin site"""
+        return self.name
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy)"""
+    title = models.CharField(max_length=200)
+
+    # Foreign key used as book can only have one author, but author can have many books
+    # Author as a string rather than an object as it hasn't declared yet
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null = True)
+
+    summary = models.TextField(max_length = 1000
+            , help_text = "Enter a brief description of the book")
+    isbn = models.CharField("ISBN", max_length= 13
+            , help_text = '13 character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    #ManyToMany as books can have multiple genres and each genre has many books
+    #Genre has been defined above, so we can specify the object instead of a string
+    genre = models.ManyToManyField(Genre, help_text = "Select a genre for the book")
+
+    language = models.ForeignKey('Language', on_delete = models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_utl(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+class BookInstance(models.Model):
+    """Model representing a specific instance of a book"""
+
+    id = models.UUIDField(primary_key = True, default=uuid.uuid4
+            , help_text = "Unique ID for this specific copy of a book")
+
+    book = models.ForeignKey("Book", on_delete = models.SET_NULL, null=True)
+    imprint = models.CharField(max_length = 200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+          ('m', "Maintenance")
+        , ('o', "On loan")
+        , ('a', "Available")
+        , ('r', "Reserved")
+    )
+
+    status = models.CharField(
+          max_length = 1
+        , choices = LOAN_STATUS
+        , blank = True
+        , default = "m"
+        , help_text = "Book availability"
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f"{self.id} ({self.book.title})"
